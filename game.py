f30e95382466c81e87cf6de72fd3f576ccf8dac1
tina <tina.amini.b@gmail.com>
2023-10-16 12:34:43 +0330

    wing movment

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ pygame.init()
 
 
 def generate_pipe_rect():
-    random_pipe = random.randrange(300,600)
+    random_pipe = random.randrange(300, 600)
     pipe_rect_top = pipe_image.get_rect(midbottom=(700, random_pipe - 200))
     pipe_rect_bottom = pipe_image.get_rect(midtop=(700, random_pipe))
     return pipe_rect_top, pipe_rect_bottom
@@ -35,9 +35,15 @@ def check_collision(pipes):
     for pipe in pipes:
         if bird_image_rect.colliderect(pipe):
             return False
-        if bird_image_rect.top <= -50 or bird_image_rect.bottom >=650:
+        if bird_image_rect.top <= -50 or bird_image_rect.bottom >= 650:
             return False
     return True
+
+
+def bird_animition():
+    new_bird = bird_list[bird_list_index]
+    new_bird_rect = new_bird.get_rect(center=(100, bird_image_rect.centery))
+    return new_bird, new_bird_rect
 
 
 # VARIABLES
@@ -49,20 +55,27 @@ bird_movement = 0
 pipe_list = []
 game_status = True
 bg_x = 0
+bird_list_index = 0
 # -------------#
 creat_pipe = pygame.USEREVENT
-pygame.time.set_timer(creat_pipe, 1200)
+creat_flap = pygame.USEREVENT +1
+pygame.time.set_timer(creat_pipe,1200)
+pygame.time.set_timer(creat_flap,100)
 # -------------#
 background_image = pygame.transform.scale(pygame.image.load("assets/img/bg2.png"), (576, 800))
-floor_image = pygame.transform.scale(pygame.image.load("assets/img/floor.png"),(576, 300))
-bird_image = pygame.transform.scale(pygame.image.load("assets/img/red_bird_mid_flap.png"), (50, 50))
-pipe_image = pygame.transform.scale(pygame.image.load('assets/img/pipe_red.png'), (100, 500))
+floor_image = pygame.transform.scale(pygame.image.load("assets/img/floor.png"), (576, 300))
 
+bird_image_down = pygame.transform.scale(pygame.image.load("assets/img/red_bird_down_flap.png"), (50, 50))
+bird_image_up = pygame.transform.scale(pygame.image.load("assets/img/red_bird_down_flap.png"), (50, 50))
+bird_image_maid = pygame.transform.scale(pygame.image.load("assets/img/red_bird_mid_flap.png"), (50, 50))
+bird_list = [bird_image_up, bird_image_maid, bird_image_down]
+bird_image = bird_list[bird_list_index]
+
+pipe_image = pygame.transform.scale(pygame.image.load('assets/img/pipe_red.png'), (100, 500))
 # rectangle
 bird_image_rect = bird_image.get_rect(center=(100, 420))
-#GAME DISPLAY
+# GAME DISPLAY
 main_screen = pygame.display.set_mode((display_width, display_height))
-
 
 # Game Timer
 clock = pygame.time.Clock()
@@ -73,21 +86,28 @@ while True:
             pygame.quit()
             # TERMINATE PROGRAM
             sys.exit()
-        #move bird
+        # move bird
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 bird_movement = 0
                 bird_movement -= 8
             if event.key == pygame.K_r and game_status == False:
-                    game_status = True
-                    pipe_list.clear()
-                    bird_image_rect.center = (100, 50)
-                    bird_movement = 0
-        if event.type == pygame.MOUSEBUTTONDOWN:
+                game_status = True
+                pipe_list.clear()
+                bird_image_rect.center = (100, 50)
                 bird_movement = 0
-                bird_movement -= 8
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            bird_movement = 0
+            bird_movement -= 8
         if event.type == creat_pipe:
-                pipe_list.extend(generate_pipe_rect())
+            pipe_list.extend(generate_pipe_rect())
+
+        if event.type == creat_flap:
+            if bird_list_index < 2 :
+                bird_list_index += 1
+            else:
+                bird_list_index = 0
+            bird_image, bird_image_rect = bird_animition()
     # display bg
     bg_x -= 0.1
     main_screen.blit(background_image, (bg_x, 0))
@@ -95,13 +115,12 @@ while True:
     if bg_x <= -576:
         bg_x = 0
 
-    #display ground
+    # display ground
     floor_x -= 1
     main_screen.blit(floor_image, (floor_x, 650))
     main_screen.blit(floor_image, (floor_x + 576, 650))
     if floor_x <= -576:
         floor_x = 0
-
 
     if game_status:
         # check collision
